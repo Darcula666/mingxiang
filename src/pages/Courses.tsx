276601be9b76f4ed7d25c33acc73f8629388c575
tiankun <1172504039@qq.com>
2025-04-08 17:56:35 +0800

    添加图片

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -11,7 +11,7 @@ const Courses: React.FC = () => {
       title: '基础呼吸',
       duration: '5分钟',
       level: '初级',
-      image: 'https://via.placeholder.com/200x120'
+      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=512x512'
     },
     {
       id: 2,
