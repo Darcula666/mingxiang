9b4a0bd705680a9efbe4f48506bba6f0f34942d8
tiankun <1172504039@qq.com>
2025-04-09 09:15:24 +0800

    11

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -11,28 +11,28 @@ const Courses: React.FC = () => {
       title: '基础呼吸',
       duration: '5分钟',
       level: '初级',
-      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=200x120'
+      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=512x512'
     },
     {
       id: 2,
       title: '专注力训练',
       duration: '10分钟',
       level: '中级',
-      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=200x120'
+      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=512x512'
     },
     {
       id: 3,
       title: '睡眠引导',
       duration: '15分钟',
       level: '初级',
-      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=200x120'
+      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=512x512'
     },
     {
       id: 4,
       title: '减压放松',
       duration: '20分钟',
       level: '中级',
-      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale=200x120'
+      image: 'https://api.redare.cn/resource/image/public/2025/01/20/678df2de7aa6ab07ac86661f@3000x4000.jpg?scale512x512'
     }
   ];
 
